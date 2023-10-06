d122b67f653da06b323c23a73ecd630d4ca42b92
0nejean <onejean114@gmail.com>
2023-10-06 16:30:20 +0900

    completed except for InteractiveEffect

--- dist/js/comm.js
+++ dist/js/comm.js
@@ -1,23 +1,51 @@
 const headerEl = document.querySelector("#header");
 
-// // 페이지에 스크롤 이벤트 추가
-// window.addEventListener(
-//   "scroll",
-//   _.throttle(function () {
-//     console.log(window.scrollY);
-//     // 페이지의 스크롤 위치가 200px보다 크면
-//     if (window.scrollY > 200) {
-//       // gsap.to(요소, 시간, 옵션)
-//       gsap.to(headerEl, 0.6, {
-//         opacity: 0,
-//         display: "none",
-//       });
-//     } else {
-//       // 페이지의 스크롤 위치가 200px보다 작으면
-//       gsap.to(headerEl, 0.6, {
-//         opacity: 1,
-//         display: "block",
-//       });
-//     }
-//   }, 300)
-// );
+// 페이지에 스크롤 이벤트 추가
+window.addEventListener(
+  "scroll",
+  _.throttle(function () {
+    console.log(window.scrollY);
+    // 페이지의 스크롤 위치가 200px보다 크면
+    if (window.scrollY > 200) {
+      // gsap.to(요소, 시간, 옵션)
+      gsap.to(headerEl, 0.6, {
+        opacity: 0,
+        display: "none",
+      });
+    } else {
+      // 페이지의 스크롤 위치가 200px보다 작으면
+      gsap.to(headerEl, 0.6, {
+        opacity: 1,
+        display: "block",
+      });
+    }
+  }, 300)
+);
+
+// Youtube IFrame API를 비동기로 로드합니다.
+var tag = document.createElement("script");
+tag.src = "https://www.youtube.com/iframe_api";
+var firstScriptTag = document.getElementsByTagName("script")[0];
+firstScriptTag.parentNode.insertBefore(tag, firstScriptTag);
+
+// onYouTubePlayerAPIReady 함수 이름은,
+// Youtube IFrame Player API에서 사용하는 이름이기 때문에,
+// 다르게 지정하면 동작하지 않습니다!
+// 그리고 함수는 전역(Global) 등록해야 합니다!
+function onYouTubePlayerAPIReady() {
+  // #player1
+  new YT.Player("player1", {
+    videoId: "-kF5ls4SJOw", // 최초 재생할 유튜브 영상 ID
+    playerVars: {
+      autoplay: true, // 자동 재생 유무
+      loop: true, // 반복 재생 유무
+      playlist: "-kF5ls4SJOw", // 반복 재생할 유튜브 영상 ID 목록
+    },
+    events: {
+      // 영상이 준비되었을 때,
+      onReady: function (event) {
+        event.target.mute(); // 음소거!
+      },
+    },
+  });
+}
